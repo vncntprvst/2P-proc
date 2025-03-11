ece09723ceb27fd15637b8ba9f66899260dbdd18
Vincent <prevosto@mit.edu>
2025-03-10 23:46:18 -0400

    add script to set up and run the analysis2p UI environment

---
+++ ui/run_app.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Define the environment directory name and archive file
+CONDA_ENV_NAME="analysis2p_ui"
+ENV_DIR=$(conda info --envs | grep "$CONDA_ENV_NAME" | awk '{print $NF}')
+ARCHIVE_FILE="analysis2p_ui.tar.gz"
+
+# If the environment folder doesn't exist, extract it
+if [ ! -d "$ENV_DIR" ]; then
+    TARGET_DIR="./portable_env"
+    # Create target directory if it doesn't exist
+    if [ ! -d "$TARGET_DIR" ]; then
+        mkdir -p "$TARGET_DIR"
+    fi
+    echo "Extracting the portable environment..."
+    tar -xzf "$ARCHIVE_FILE" -C "$TARGET_DIR"
+    # After extraction, the environment folder will be inside TARGET_DIR
+    ENV_DIR="$TARGET_DIR/$CONDA_ENV_NAME"
+    # Fix paths inside the extracted environment
+    "$ENV_DIR/bin/conda-unpack"
+
+    # Activate the environment
+    source "$ENV_DIR/bin/activate"  
+else
+    echo "Found environment directory: $ENV_DIR"
+    # Get the base environment directory
+    BASE_ENV_DIR=$(conda info --base)
+    echo "Base environment directory: $BASE_ENV_DIR"
+
+    # Source the conda initialization script
+    source "$BASE_ENV_DIR/etc/profile.d/conda.sh"
+
+    # Activate the environment
+    conda activate analysis2p_ui
+fi
+
+echo "Running the app..."
+# python ui_app.py
+streamlit run ui_app.py
+
+# Optionally deactivate at the end
+# deactivate
