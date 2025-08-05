1e934a7f51ac2b58d4aec9d90230cbe51bcf819a
Vincent <prevosto@mit.edu>
2025-08-05 12:30:44 -0400

    create suite2p image for v0.14.0 stable release

---
+++ containers/suite2p/build_s2p_stable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t wanglabneuro/suite2p:latest -t wanglabneuro/suite2p:v0.14.0 -f Dockerfile_s2p_stable .
+
+# Check Suite2p version:
+# docker run --rm -it wanglabneuro/suite2p:latest python -c "import suite2p; print(suite2p.__version__)"
